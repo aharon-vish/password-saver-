d18ec8493cac6085abe62bd7808c770b87cfc6a8
aharon-vish <aharon.vish@gmail.com>
2017-07-25 20:55:18 +0300

    fnish style animated placeholder

--- saveYourPassword/src/actions/AuthAction.js
+++ saveYourPassword/src/actions/AuthAction.js
@@ -2,16 +2,16 @@ import {LOGIN_USER, LOGIN_USER_FAIL, REGISTRATION_USER} from '../types';
 import firebase from 'firebase';
 import {Actions} from 'react-native-router-flux';
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
     if (!user) {
         loginUserFail();
     }
 });
 
 export const loginUser = (email, password)=> {
-     const request =
-         firebase.auth()
-             .signInWithEmailAndPassword(email, password);
+    const request =
+        firebase.auth()
+            .signInWithEmailAndPassword(email, password);
     return {
         type: LOGIN_USER,
         payload: request
@@ -27,8 +27,10 @@ export const registrationUser = (email, password)=> {
     };
 };
 const loginUserFail = ()=> {
-    Actions.auth();
-    dispatch({
-        type: LOGIN_USER_FAIL
-    });
+    return (dispatch)=> {
+        Actions.auth();
+        dispatch({
+            type: LOGIN_USER_FAIL
+        });
+    }
 };
--- saveYourPassword/src/components/common/Form.js
+++ saveYourPassword/src/components/common/Form.js
@@ -1,49 +1,83 @@
-import React,{Component}from 'react';
-import {View,Text,Animated,Easing} from 'react-native';
-import {FormLabel, FormInput,Button,FormValidationMessage} from 'react-native-elements';
+import React, {Component}from 'react';
+import {View, Text, Animated, Easing} from 'react-native';
+import {FormLabel, FormInput, Button, FormValidationMessage} from 'react-native-elements';
 
 class Form extends Component {
     constructor(props) {
         super(props);
-        const {label,placeholder,onChangeText,onBlur,fieldName,secureTextEntry} = this.props;
-        this.state={label,placeholder,onChangeText,onBlur,fieldName,secureTextEntry,
-                     fadeAnim: new Animated.Value(0.5),
-                     topAnim:new Animated.Value(30)};
+        this.state = {
+            inputValue:'',
+            fadeAnim: new Animated.Value(0.5),
+            topAnim: new Animated.Value(30),
+            leftAnim: new Animated.Value(20),
+        };
         this.startAnimated = this.startAnimated.bind(this);
+        this.resetAnimated = this.resetAnimated.bind(this);
     }
+
     componentDidMount() {
     }
 
-    startAnimated(){
+    startAnimated() {
         Animated.parallel([
             Animated.timing(
-                this.state.topAnim,{
+                this.state.topAnim, {
                     toValue: 0,
                     duration: 200
                 }),
             Animated.timing(
-                this.state.fadeAnim,{
+                this.state.leftAnim, {
+                    toValue: 10,
+                    duration: 200
+                }),
+            Animated.timing(
+                this.state.fadeAnim, {
                     toValue: 1,
                     duration: 200
                 })
         ]).start();
     }
-    onFocus() {
-        this.startAnimated();
+
+    resetAnimated() {
+        this.state.inputValue === '' &&
+        Animated.parallel([
+            Animated.timing(
+                this.state.topAnim, {
+                    toValue: 30,
+                    duration: 200
+                }),
+            Animated.timing(
+                this.state.leftAnim, {
+                    toValue: 20,
+                    duration: 200
+                }),
+            Animated.timing(
+                this.state.fadeAnim, {
+                    toValue: 0.5,
+                    duration: 200
+                })
+        ]).start();
     }
 
     render() {
-        let { fadeAnim,topAnim} = this.state;
+        const {label, placeholder, onChangeText, onBlur, fieldName, secureTextEntry} = this.props;
+        let {fadeAnim, topAnim,leftAnim} = this.state;
         return (
             <View>
-                <Animated.Text style={{left:10, top: topAnim,opacity: fadeAnim}}>{this.state.label}
+                <Animated.Text style={{left: leftAnim, top: topAnim, opacity: fadeAnim}}>
+                    {label}
                 </Animated.Text>
 
                 <FormInput
                     onFocus={this.startAnimated}
-                    secureTextEntry={this.state.secureTextEntry}
-                    onBlur={() => onBlur(this.state.fieldName)}
-                    onChangeText={this.state.onChangeText}/>
+                    secureTextEntry={secureTextEntry}
+                    onBlur={() => {
+                        onBlur(fieldName);
+                        this.resetAnimated();
+                    }}
+                    onChangeText={(value) => {
+                        onChangeText(value);
+                        this.setState({inputValue:value})}}/>
             </View>
         )
     }
